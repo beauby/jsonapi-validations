9fa97ff1b5d86f8b6d242f5b1001ee712a656149
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-10-02 23:27:04 +0200

    Fix unfinished extraction from parser (stale reference to Document.ensure!).

--- lib/jsonapi/validations/relationship.rb
+++ lib/jsonapi/validations/relationship.rb
@@ -11,7 +11,7 @@ module JSONAPI
       # @param [Hash] params Validation parameters.
       #   @option [Symbol] kind Whether it is a :has_many or :has_one.
       #   @option [Array<Symbol>] types Permitted types for the relationship.
-      # @raise [JSONAPI::Validator::InvalidDocument] if document is invalid.
+      # @raise [JSONAPI::Parser::InvalidDocument] if document is invalid.
       def validate_relationship!(document, params = {})
         JSONAPI.parse_relationship!(document)
         validate_types!(document['data'], params[:types])
@@ -21,23 +21,29 @@ module JSONAPI
       def validate_types!(rel, rel_types, key = nil)
         rel_name = key ? " #{key}" : ''
         if rel_types[:kind] == :has_many
-          Document.ensure!(rel['data'].is_a?(Array),
-                           "Expected relationship#{rel_name} to be has_many.")
+          unless rel['data'].is_a?(Array)
+            raise JSONAPI::Parser::InvalidDocument,
+                  "Expected relationship#{rel_name} to be has_many."
+          end
           return unless rel_types[:types]
           rel['data'].each do |ri|
-            Document.ensure!(rel_types[:types].include?(ri['type'].to_sym),
-                             "Type mismatch for relationship#{rel_name}: " \
-                             "#{ri['type']} should be one of #{rel_types}")
+            next if rel_types[:types].include?(ri['type'].to_sym)
+            raise JSONAPI::Parser::InvalidDocument,
+                  "Type mismatch for relationship#{rel_name}: " \
+                  "#{ri['type']} should be one of #{rel_types}"
           end
         else
           return if rel['data'].nil?
-          Document.ensure!(rel['data'].is_a?(Hash),
-                           "Expected relationship#{rel_name} to be has_one.")
+          unless rel['data'].is_a?(Hash)
+            raise JSONAPI::Parser::InvalidDocument,
+                  "Expected relationship#{rel_name} to be has_one."
+          end
           return unless rel_types[:types]
           ri = rel['data']
-          Document.ensure!(rel_types[:types].include?(ri['type'].to_sym),
-                           "Type mismatch for relationship#{rel_name}: " \
-                           "#{ri['type']} should be one of #{rel_types}")
+          return if rel_types[:types].include?(ri['type'].to_sym)
+          raise JSONAPI::Parser::InvalidDocument,
+                "Type mismatch for relationship#{rel_name}: " \
+                "#{ri['type']} should be one of #{rel_types}"
         end
       end
     end
--- lib/jsonapi/validations/resource.rb
+++ lib/jsonapi/validations/resource.rb
@@ -19,7 +19,7 @@ module JSONAPI
       #   @option [Hash] types Permitted primary/relationships types. Optional.
       #     The relationships must be explicitly permitted. Not all
       #     relationships' types have to be specified.
-      # @raise [JSONAPI::Validator::InvalidDocument] if document is invalid.
+      # @raise [JSONAPI::Parser::InvalidDocument] if document is invalid.
       #
       # @example
       #   params = {
@@ -58,54 +58,65 @@ module JSONAPI
       # @api private
       def validate_permitted!(data, permitted)
         return if permitted.nil?
-        Document.ensure!(permitted[:id] || !data.key?('id'),
-                         'Unpermitted id.')
+        unless permitted[:id] || !data.key?('id')
+          raise JSONAPI::Parser::InvalidDocument,
+                'Unpermitted id.'
+        end
         # TODO(beauby): Handle meta (and possibly links) once the spec has
         #   been clarified.
         permitted_attrs = permitted[:attributes] || []
         if data.key?('attributes')
           data['attributes'].keys.each do |attr|
-            Document.ensure!(permitted_attrs.include?(attr.to_sym),
-                             "Unpermitted attribute #{attr}")
+            unless permitted_attrs.include?(attr.to_sym)
+              raise JSONAPI::Parser::InvalidDocument,
+                    "Unpermitted attribute #{attr}"
+            end
           end
         end
         permitted_rels = permitted[:relationships] || []
-        if data.key?('relationships')
-          data['relationships'].keys.each do |rel|
-            Document.ensure!(permitted_rels.include?(rel.to_sym))
-          end
+        return unless  data.key?('relationships')
+        data['relationships'].keys.each do |rel|
+          next if permitted_rels.include?(rel.to_sym)
+          raise JSONAPI::Parser::InvalidDocument
         end
       end
 
       # @api private
       def validate_required!(data, required)
         return if required.nil?
-        Document.ensure!(data.key?('id') || !required[:id],
-                         'Missing required id.')
+        unless data.key?('id') || !required[:id]
+          raise JSONAPI::Parser::InvalidDocument, 'Missing required id.'
+        end
         # TODO(beauby): Same as for permitted.
 
-        Document.ensure!(data.key?('attributes') || !required[:attributes],
-                         'Missing required attributes.')
-        required[:attributes].each do |attr|
-          Document.ensure!(data['attributes'][attr.to_s],
-                           "Missing required attribute #{attr}.")
+        unless data.key?('attributes') || !required[:attributes]
+          raise JSONAPI::Parser::InvalidDocument, 'Missing required attributes.'
         end
-        Document.ensure!(data.key?('relationships') ||
-                         !required[:relationships],
-                         'Missing required relationships.')
+        required[:attributes].each do |attr|
+          next if data['attributes'][attr.to_s]
+          raise JSONAPI::Parser::InvalidDocument,
+                "Missing required attribute #{attr}."
+        end
+        unless data.key?('relationships') || !required[:relationships]
+          raise JSONAPI::Parser::InvalidDocument,
+                'Missing required relationships.'
+        end
         required[:relationships].each do |rel|
-          Document.ensure!(data['relationships'][rel.to_s],
-                           "Missing required relationship #{rel}.")
+          unless data['relationships'][rel.to_s]
+            raise JSONAPI::Parser::InvalidDocument,
+                  "Missing required relationship #{rel}."
+          end
         end
       end
 
       # @api private
       def validate_types!(data, types)
         return if types.nil?
-        Document.ensure!(!types[:primary] ||
-                         types[:primary].include?(data['type'].to_sym),
-                         "Type mismatch for resource: #{data['type']} " \
-                         "should be one of #{types[:primary]}")
+        unless !types[:primary] || types[:primary].include?(data['type'].to_sym)
+          raise JSONAPI::Parser::InvalidDocument,
+                "Type mismatch for resource: #{data['type']} " \
+                "should be one of #{types[:primary]}"
+        end
         return unless data.key?('relationships') && types.key?(:relationships)
         types[:relationships].each do |key, rel_types|
           rel = data['relationships'][key.to_s]
